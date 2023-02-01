fcf88d2a61104b04614c42f595e1d2bf1d6cee5e
minemotok <soccer.four35@gmail.com>
2023-02-01 18:51:57 +0900

    お問い合わせ機能等追加

---
+++ my_app/footer/inquiry/complete/complete.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせフォーム完了画面</title>
+    <link rel="stylesheet" href="complete.css">
+    <script src="https://kit.fontawesome.com/4e00f87af3.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row complete_title">
+        <div class="col title_text">
+          <h1>送信完了</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col arrow">
+          <span>入力</span>
+          <span>確認</span>
+          <span id="complete">完了</span>
+        </div>
+      </div>
+
+      <div class="row complete_icon">
+        <div class="col">
+          <i class="fa-regular fa-circle-check"></i>
+        </div>
+      </div>
+
+      <div class="row complete_text">
+        <div class="col">
+          <p id="text_head">お問い合わせ完了しました</p>
+          <p id="text_body">
+            お問い合わせいただきありがとうございます</br>
+            内容を確認次第、ご対応させていただきますのでもうしばらくお待ちください
+          </p>
+        </div>
+      </div>
+
+      <div class="row back_home">
+        <div class="col">
+          <a href="../../../main.php" class="link_abbsolute">
+            <span>ホームへ戻る</span>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </body>
+
+</html>
---
+++ my_app/footer/inquiry/confirm/confirmation.php
@@ -0,0 +1,172 @@
+<?php
+$host = $_SERVER['HTTP_HOST'];
+if (!empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], $host) !== false) {
+  $refere = $_SERVER['HTTP_REFERER'];
+} else {
+  $dir = dirname($_SERVER['PHP_SELF'], 2);
+  header("Location: //$host$dir/inquiry.php");
+}
+$company_name = $_POST['company_name'];
+$username = $_POST['username'];
+$birth_day = $_POST['birth_day'];
+$mailaddress = $_POST['mailaddress'];
+$post = $_POST['post'];
+$prefecture = $_POST['prefecture'];
+$address = $_POST['address'];
+$contents = $_POST['contents'];
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ内容確認画面</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="confirmation.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4e00f87af3.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <!-- header部分 -->
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>お問い合わせ</h1>
+        </div>
+      </div>
+
+      <div class="row confirm_text">
+        <div class="col-1">
+          <i class="fa-solid fa-circle-exclamation"></i>
+        </div>
+        <div class="col-11 element_item">
+          <p>まだ、お問い合わせ入力は完了していません</p>
+          <p>入力内容をご確認ください</p>
+          <p>この内容でお問い合わせする場合は、送信ボタンを押してください</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col arrow">
+          <span>入力</span>
+          <span id="confirmation">確認</span>
+          <span>完了</span>
+        </div>
+      </div>
+
+      <form action="../complete/complete.php" method="POST">
+        <div class="row frame">
+          <div class="col framework">
+            <table>
+              <tr class="separate_blue">
+                <td class="title"><span>法人/個人 <span class="gray">【任意】</span></span></td>
+                <td class="contents"><span><?php if (isset($job_category) === false) {
+                                            echo "";
+                                          } else {
+                                            echo $job_category;
+                                          } ?></span></td>
+              </tr>
+              <tr class="separate_sky">
+                <td class="title"><span>会社名 <span class="skin">【法人の場合のみ】</span></span></td>
+                <td class="contents"><span>
+                    <?php if (isset($company_name) === false) {
+                    echo "";
+                  } else {
+                    echo $company_name;
+                  } ?> </span></td>
+              </tr>
+              <tr class="separate_blue">
+                <td class="title"><span>氏名 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $username ?></span></td>
+              </tr>
+              <tr class="separate_sky">
+                <td class="title"><span>性別 <span class="gray">【任意】</span></span></td>
+                <td class="contents"><span><?php if (isset($gender) === false) {
+                                            echo "";
+                                          } else {
+                                            echo $gender;
+                                          } ?></span></td>
+              </tr>
+              <tr class="separate_blue">
+                <td class="title"><span>生年月日 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $birth_day ?></span></td>
+              </tr>
+              <tr class="separate_sky">
+                <td class="title"><span>メールアドレス <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $mailaddress ?></span></td>
+              </tr>
+              <tr class="separate_blue">
+                <td class="title"><span>郵便番号 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $post ?></span></td>
+              </tr>
+              <tr class="separate_sky">
+                <td class="title"><span>都道府県 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $prefecture ?></span></td>
+              </tr>
+              <tr class="separate_blue">
+                <td class="title"><span>住所 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $address ?></span></td>
+              </tr>
+              <tr class="separate_sky">
+                <td id="content_area"><span>お問い合わせ内容 <span class="red">【必須】</span></span></td>
+                <td class="contents"><span><?= $contents ?></span></td>
+              </tr>
+            </table>
+          </div>
+
+          <div class="row submit_button">
+            <div class="col-3">
+              <button type="button" id="back_button">
+                <?php
+              echo '<a id="back" href="' . $refere . '">戻る</a>';
+              ?>
+              </button>
+            </div>
+            <div class="col">
+              <input type="submit" value="送信" name="localstorage_submit" id="submitButton">
+            </div>
+          </div>
+        </div>
+
+      </form>
+  </body>
+
+  <!-- ローカルストレージに格納する -->
+  <script>
+  const submit = document.getElementById('submitButton');
+  submit.addEventListener('click', function() {
+    let inquiry_data = {
+      job_category: '<?php if (isset($_POST['job_category']) === false) {
+                        echo "";
+                      } else {
+                        echo $_POST['job_category'];
+                      } ?>',
+      company_name: '<?php if (isset($company_name) === false) {
+                        echo "";
+                      } else {
+                        echo $company_name;
+                      } ?>',
+      username: '<?= $username ?>',
+      gender: '<?php if (isset($_POST['gender']) === false) {
+                  echo "";
+                } else {
+                  echo $_POST['gender'];
+                } ?>',
+      birth_day: '<?= $birth_day ?>',
+      mailaddress: '<?= $mailaddress ?>',
+      post: '<?= $post ?>',
+      prefecture: '<?= $prefecture ?>',
+      address: '<?= $address ?>',
+      contents: '<?= $contents ?>'
+    }
+
+    let json = JSON.stringify(inquiry_data);
+    localStorage.setItem('input_data', json);
+  });
+  </script>
+
+</html>
